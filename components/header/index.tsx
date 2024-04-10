b21219148ebbbd24ae8a00e71e4f9b8cd059de63
sanixto <sanix.to@gmail.com>
2024-04-10 17:39:38 +0300

    add HeaderBackground component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -3,20 +3,24 @@ import Image from 'next/image';
 
 import logoImg from '@/assets/logo.png';
 import styles from './index.module.css';
+import HeaderBackground from './header-background';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-          <Image src={logoImg} alt="A plate with food on it" />
-          NextLevel Food
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li><Link href="/meals">Browse Meals</Link></li>
-          <li><Link href="/community">Foodies Community</Link></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+    <HeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+            <Image src={logoImg} alt="A plate with food on it" />
+            NextLevel Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li><Link href="/meals">Browse Meals</Link></li>
+            <li><Link href="/community">Foodies Community</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
   )
 }

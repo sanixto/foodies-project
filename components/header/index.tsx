8dcd29fbb0c93f097dfeeb4cd190f43a28c800c4
sanixto <sanix.to@gmail.com>
2024-04-10 17:07:21 +0300

    add styles to Header component

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,15 +1,16 @@
 import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
+import styles from './index.module.css';
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
           <img src={logoImg.src} alt="A plate with food on it" />
           NextLevel Food
       </Link>
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li><Link href="/meals">Browse Meals</Link></li>
           <li><Link href="/community">Foodies Community</Link></li>

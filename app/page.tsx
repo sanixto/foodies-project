56528aa391a069277dd7cad29324f9e27346a102
sanixto <sanix.to@gmail.com>
2024-04-23 18:21:47 +0300

    add slide show to Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 
 import styles from './page.module.css';
+import ImageSlideshow from '@/components/images/image-slideshow';
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
         <div className={styles.slideshow}>
-
+          <ImageSlideshow />
         </div>
         <div>
           <div className={styles.hero}>

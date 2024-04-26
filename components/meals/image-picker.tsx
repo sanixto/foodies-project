4d952424de422b5dfcc2e00c0b2c91fde695d227
sanixto <sanix.to@gmail.com>
2024-04-26 12:47:06 +0300

    add Image Preview to Image Picker

--- components/meals/image-picker.tsx
+++ components/meals/image-picker.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useRef } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
+import Image from 'next/image';
+
 import styles from './image-picker.module.css';
 
 interface ImagePickerProps {
@@ -9,16 +11,41 @@ interface ImagePickerProps {
 }
 
 export default function ImagePicker({ label, name }: ImagePickerProps) {
+  const [pickedImage, setPickedImage] = useState<string | null>(null);
   const imageInputRef: React.RefObject<HTMLInputElement> | undefined = useRef<HTMLInputElement>(null);
 
   const handlePickClick = () => {
     imageInputRef.current?.click();
   };
 
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target;
+    const file: File | undefined = files ? files[0]: undefined;
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      const { result } = fileReader;
+      if (result) setPickedImage(result.toString());
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={styles.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={styles.controls}>
+        <div className={styles.preview}>
+          {pickedImage ? (
+            <Image src={pickedImage} alt="The image selected by the user." fill />
+          ): <p>No image picked yet.</p>}
+        </div>
         <input
           className={styles.input}
           type="file"
@@ -26,6 +53,8 @@ export default function ImagePicker({ label, name }: ImagePickerProps) {
           id={name}
           accept="image/png, image/jpeg"
           ref={imageInputRef}
+          onChange={handleImageChange}
+          required
         />
         <button
           className={styles.button}
